bafa6e5382e82669e607adfab105d1c1082030b5
Mikelesnr <mwanza.n.m@gmail.com>
2023-01-07 12:03:25 +0200

    dploy script edit

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "Building app..."
 npm run build
 
 echo "Deploying files to server..."
-scp -r build/* mikelsnr@188.166.81.83:/var/www/pocket-geologist/
+scp -r build/* root@188.166.81.83:/var/www/pocket-geologist/
 
 echo "done"
